bedcc77959c08369a5bcee76f144b8945176558c
denisbahnari <denisbahnari042@gmail.com>
2025-12-26 02:38:40 +0000

    persistence memory chat

--- cf-ai-code-assistant/src/AssistantDO.ts
+++ cf-ai-code-assistant/src/AssistantDO.ts
@@ -1,27 +1,46 @@
 import { DurableObject } from "cloudflare:workers";
 
+interface Chat {
+    question: string,
+    answer: string
+}
+
 interface AssistantState {
     sessionName: string;
     languages: string[];
-    chatHistory: string[];
+    chatHistory: Chat[];
 }
 
 export class AssistantDO extends DurableObject<Env> {
+
+    private static readonly CHAT_HISTORY_LENGTH = 7; 
+
+    state: DurableObjectState;
     stateData: AssistantState;
 
     constructor(state: DurableObjectState, env: Env) {
         super(state, env);
+        this.state = state;
+
         this.stateData = {
             sessionName: "defaultName",
             languages: ["python"],
             chatHistory: []
         }
+
+        state.blockConcurrencyWhile(async () => {
+        const stored = await state.storage.get<AssistantState>("stateData");
+        if (stored) {
+            this.stateData = stored;
+        }
+    });
     }
 
     async askAI(question: string): Promise<string> {
         const response = await this.env.AI.run("@cf/meta/llama-3-8b-instruct", {
             messages: [
                 {role: "system", content: "You are Rob, a fast conding assistant"},
+                ...this.getChatHistoryFormatted(AssistantDO.CHAT_HISTORY_LENGTH),
                 {role: "user", content: question}
             ]
         });
@@ -34,12 +53,27 @@ export class AssistantDO extends DurableObject<Env> {
         if (!requestedText.toLowerCase().includes("hey rob")) {
             return new Response(null, {status:204});
         }
+        const answer = await this.askAI(requestedText);
 
-        const question = requestedText.replace("/hey rob/i", "").trim();
-        const awnser = await this.askAI(requestedText);
-        this.stateData.chatHistory.push(requestedText);
+        this.stateData.chatHistory.push({
+            question: requestedText,
+            answer: answer
+        });
+        await this.state.storage.put("stateData", this.stateData);
 
-        return new Response(awnser);
+        console.log(this.getChatHistoryFormatted(3));
+        
+        return new Response(answer);
     }
+
+    private getChatHistoryFormatted(n: number) {
+        return this.stateData.chatHistory
+            .slice(-n)
+            .flatMap(entry => [
+                { role: "user", content: entry.question },
+                { role: "assistant", content: entry.answer }
+            ]);
+    }
+
 
 }
