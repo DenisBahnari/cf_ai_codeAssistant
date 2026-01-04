d736f818d2a081bec2a92e558deecf37b6b4a803
denisbahnari <denisbahnari042@gmail.com>
2026-01-04 18:44:20 +0000

    GET agent state API for more frontend

--- cf-ai-code-assistant/src/index.ts
+++ cf-ai-code-assistant/src/index.ts
@@ -111,6 +111,17 @@ export default {
 			}
 		}
 
+		if (request.method === "GET" && url.pathname == "/agent_status") {
+			const sessionId = await request.headers.get("x-session-id");
+			if (sessionId != null) {
+				const id = env.MY_DURABLE_OBJECT.idFromName(sessionId);
+				const stub = env.MY_DURABLE_OBJECT.get(id);
+				return await stub.fetch(request);
+			} else {
+				return new Response("Null Session Id", {status: 500})
+			}
+		}
+
 		if (request.method === "POST" && url.pathname === "/chat") {
 			const sessionId = await request.headers.get("x-session-id");
 			if (sessionId != null) {
